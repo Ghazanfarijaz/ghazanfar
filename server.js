25a2e382d25530eb04da9a0a1a77ce6d9a0f6556
Ghazanfar Ijaz <95541056+MGhazanfarijaz@users.noreply.github.com>
2022-10-16 13:38:05 +0500

    get and post

--- server.js
+++ server.js
@@ -9,23 +9,41 @@ dotenv.config();
 
 
 const connect = async () => {
-    try {
-      await mongoose.connect(process.env.MONGO);
-      console.log("Connected to mongoDB.");
-    } catch (error) {
-      throw error;
-    }
-  };
-  
-  mongoose.connection.on("disconnected", () => {
-    console.log("mongoDB disconnected!");
-  });
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB.");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!");
+});
 mongoose.connection.on("connected", () => {
     console.log("mongoDB connected!");
   });
   
-
-
+  app.get("/",function(req,res){
+      
+    res.sendFile(__dirname+"/public.html");
+    
+     
+    
+    })
+    
+    app.post("/",function(req,res){
+    
+     var user = new userModel({
+      name: req.body.uname
+       });
+  
+      user.save();
+      
+      res.send("<h1>Your username '"+req.body.uname+"' has been stored in the DB. </h1>")
+     
+    
+    })
     
     app.listen(3000, () => {
       connect();
